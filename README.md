af4774b8c570908c3faff5b58723050ee49932ef
Stefan Horst <stefan.horst@grandcentrix.net>
2019-06-01 21:13:24 +0200

    now with apt bundle

---
+++ README.md
@@ -0,0 +1,5 @@
+This Bundle will install Docker from the docker debian repository
+
+h4 Requires
+This Bundle requires https://github.com/sHorst/bw.bundle.apt to install all required Packages
+
--- items.py
+++ items.py
@@ -1,5 +1,5 @@
-COMPOSE_VER = '1.16.1'
-COMPOSE_SUM = 'e2c7c848e1fa388a2e5b8945fdb2660bf8d8adb1'
+COMPOSE_VER = '1.24.0'
+COMPOSE_SUM = 'bee6460f96339d5d978bb63d17943f773e1a140242dfa6c941d5e020a302c91b'
 
 pkg_apt = {
     'docker-ce': {
@@ -33,12 +33,22 @@ files = {
 actions = {
     'docker_install_apt_key': {
         'command': 'curl -L https://download.docker.com/linux/debian/gpg | apt-key add -',
-        'unless': 'apt-key list | grep "0EBF CD88"',
         'needs': ['pkg_apt:curl'],
+        'unless': 'apt-key list | grep "Docker Release (CE deb) <docker@docker.com>"'
     },
-    'download_docker-compose': {
-        'command': 'curl -L https://github.com/docker/compose/releases/download/{}/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose'.format(COMPOSE_VER),
-        'unless': 'sha1sum /usr/local/bin/docker-compose| grep -q ^{}'.format(COMPOSE_SUM),
-        'needs': ['pkg_apt:curl'],
+    'make_executable': {
+        'command': 'chmod +x /usr/local/bin/docker-compose',
+        'triggered': True,
+    }
+}
+
+downloads = {
+    '/usr/local/bin/docker-compose': {
+        'url': 'https://github.com/docker/compose/releases/download/{version}/docker-compose-Linux-x86_64'.format(
+            version=COMPOSE_VER,
+        ),
+        'sha256': COMPOSE_SUM,
+        'needs': ['pkg_apt:ca-certificates'],
+        'triggers': ['action:make_executable'],
     }
 }
