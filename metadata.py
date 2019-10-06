ae7a0cda50ff26111686f12f877b421d28cd8b08
Stefan Horst <stefan@ultrachaos.de>
2019-10-06 19:33:41 +0200

    add docker group to all users

--- metadata.py
+++ metadata.py
@@ -8,3 +8,20 @@ def add_apt_packages(metadata):
         metadata['apt']['packages']['ca-certificates'] = {'installed': True}
 
     return metadata, DONE
+
+
+@metadata_processor
+def add_sudo_group_processor(metadata):
+    if 'users' in metadata:
+        for username in metadata['users'].keys():
+            if metadata['users'][username].get('docker', False):
+                add_groups = metadata['users'][username].get('add_groups', [])
+
+                if 'docker' not in add_groups:
+                    add_groups.append('docker')
+
+                metadata['users'][username]['add_groups'] = add_groups
+
+        return metadata, DONE
+
+    return metadata, RUN_ME_AGAIN
